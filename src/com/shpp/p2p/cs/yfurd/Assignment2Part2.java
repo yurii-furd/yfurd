9a48062ca6d38cad1b6373beec497aa1b7864c9b
Yurii Furd <yura.furd@gmail.com>
2020-11-09 15:14:09 +0200

    Level2 Task2

---
+++ src/com/shpp/p2p/cs/yfurd/Assignment2Part2.java
@@ -0,0 +1,49 @@
+package com.shpp.p2p.cs.yfurd;
+
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import com.shpp.cs.a.graphics.WindowProgram;
+
+import java.awt.*;
+
+public class Assignment2Part2 extends WindowProgram {
+
+    public static final int APPLICATION_WIDTH = 300;
+    public static final int APPLICATION_HEIGHT = 300;
+    private static final int DIAMETER = 200;
+
+    public void run() {
+        GOval gOval = new GOval(APPLICATION_WIDTH, APPLICATION_HEIGHT, DIAMETER, DIAMETER);
+        gOval.setColor(Color.BLACK);
+        gOval.setFilled(true);
+        gOval.setFillColor(Color.BLACK);
+        add(gOval);
+
+        GOval gOval2 = new GOval(APPLICATION_WIDTH - APPLICATION_WIDTH, APPLICATION_HEIGHT, DIAMETER, DIAMETER);
+        gOval2.setColor(Color.BLACK);
+        gOval2.setFilled(true);
+        gOval2.setFillColor(Color.BLACK);
+        add(gOval2);
+
+        GOval gOval3 = new GOval(APPLICATION_WIDTH, APPLICATION_HEIGHT - APPLICATION_HEIGHT, DIAMETER, DIAMETER);
+        gOval3.setColor(Color.BLACK);
+        gOval3.setFilled(true);
+        gOval3.setFillColor(Color.BLACK);
+        add(gOval3);
+
+        GOval gOval4 = new GOval(APPLICATION_WIDTH - APPLICATION_WIDTH, APPLICATION_HEIGHT - APPLICATION_HEIGHT, DIAMETER, DIAMETER);
+        gOval4.setColor(Color.BLACK);
+        gOval4.setFilled(true);
+        gOval4.setFillColor(Color.BLACK);
+        add(gOval4);
+
+        GRect gRect = new GRect(DIAMETER/2, DIAMETER/2, APPLICATION_WIDTH, APPLICATION_HEIGHT);
+        gRect.setColor(Color.WHITE);
+        gRect.setFilled(true);
+        gRect.setFillColor(Color.WHITE);
+        add(gRect);
+    }
+
+}
+
+
