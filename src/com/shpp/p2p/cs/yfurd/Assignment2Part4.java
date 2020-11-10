1830eba3f77a38e5f2d91f42dc6a9f33f5421563
Yurii Furd <yura.furd@gmail.com>
2020-11-10 20:43:42 +0200

    Refactor

--- src/com/shpp/p2p/cs/yfurd/Assignment2Part4.java
+++ src/com/shpp/p2p/cs/yfurd/Assignment2Part4.java
@@ -5,28 +5,39 @@ import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
 
+import static java.lang.Thread.sleep;
+
 public class Assignment2Part4 extends WindowProgram {
 
-    public static final int APPLICATION_WIDTH = 100;
-    public static final int APPLICATION_HEIGHT = 100;
-    public static final int STRIP_WIDTH = APPLICATION_WIDTH / 5;
-    public static final int STRIP_HEIGHT = (APPLICATION_HEIGHT / 5) * 4;
+    public static final int APPLICATION_WIDTH = 1000;
+    public static final int APPLICATION_HEIGHT = 1000;
+    public static final int STRIP_WIDTH = 100;
+    public static final int STRIP_HEIGHT = 300;
+
 
     public void run() {
+        try {
+            sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        GRect gRect = new GRect(APPLICATION_WIDTH / 5, APPLICATION_HEIGHT / 10, STRIP_WIDTH, STRIP_HEIGHT);
+        int middleX = getWidth()/2 - STRIP_WIDTH/2;
+        int middleY = getHeight()/2 - STRIP_HEIGHT/2;
+
+        GRect gRect = new GRect(middleX, middleY, STRIP_WIDTH, STRIP_HEIGHT);
         gRect.setFilled(true);
-        gRect.setFillColor(Color.BLACK);
-        gRect.setColor(Color.BLACK);
+        gRect.setFillColor(Color.YELLOW);
+        gRect.setColor(Color.YELLOW);
         add(gRect);
 
-        GRect gRect2 = new GRect(APPLICATION_WIDTH / 5 + STRIP_WIDTH, APPLICATION_HEIGHT / 10, STRIP_WIDTH, STRIP_HEIGHT);
+        GRect gRect2 = new GRect(middleX - STRIP_WIDTH, middleY, STRIP_WIDTH, STRIP_HEIGHT);
         gRect2.setFilled(true);
-        gRect2.setFillColor(Color.YELLOW);
-        gRect2.setColor(Color.YELLOW);
+        gRect2.setFillColor(Color.BLACK);
+        gRect2.setColor(Color.BLACK);
         add(gRect2);
 
-        GRect gRect3 = new GRect(APPLICATION_WIDTH / 5 + STRIP_WIDTH * 2, APPLICATION_HEIGHT / 10, STRIP_WIDTH, STRIP_HEIGHT);
+        GRect gRect3 = new GRect(middleX + STRIP_WIDTH, middleY, STRIP_WIDTH, STRIP_HEIGHT);
         gRect3.setFilled(true);
         gRect3.setFillColor(Color.RED);
         gRect3.setColor(Color.RED);
