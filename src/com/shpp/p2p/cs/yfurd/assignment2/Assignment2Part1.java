23826ad24ad535b7fa272506a4b8935b475288d4
Yurii Furd <yura.furd@gmail.com>
2020-11-12 13:58:08 +0200

    Refactor

--- src/com/shpp/p2p/cs/yfurd/assignment2/Assignment2Part1.java
+++ src/com/shpp/p2p/cs/yfurd/assignment2/Assignment2Part1.java
@@ -16,49 +16,68 @@ public class Assignment2Part1 extends TextProgram {
         if (a == 0) {
             System.out.println("This is not a quadratic equation");
         } else {
-
             // If b and c = 0  the equation has no roots.
-            if (b == 0 && c == 0) {
-                System.out.println("There is one root: 0");
-            }
+            ifBAndCIsEqualToNull(b, c);
 
             // If only c = 0 the quadratic equation has two roots
-            if (b != 0 && c == 0) {
-                double root1 = 0;
-                double root2 = -b / a;
-                System.out.println("There are two roots: " + root1 + " and " + root2);
-            }
+            ifOnlyCEqualToNull(a, b, c);
 
             // If only b = 0
-            if (b == 0 && c != 0) {
-                // and square root with -b / a more than 0, the quadratic equation has two roots.
-                if (Math.sqrt(-b / a) > 0) {
-                    double root1 = Math.sqrt(-c / a);
-                    double root2 = -Math.sqrt(-c / a);
-                    System.out.println("There are two roots: " + root1 + " and " + root2);
-                // If square root with -b / a few than 0, the quadratic are no real roots.
-                } else {
-                    System.out.println("There are no real roots");
-                }
-            }
+            ifOnlyBEqualToNull(a, b, c);
 
             //If all values are not equal to 0
-            if (a !=0 && b != 0 && c != 0 ){
-                //calculate the discriminant.
-                double discriminant = b * b - 4 * a * c;
+            ifAllIsNotNull(a, b, c);
+        }
+    }
 
-                if (discriminant < 0) {
-                    println("There are no real roots");
-                    // if discriminant more than zero, he calculate the rots
+    // If b and c = 0  the equation has no roots.
+    private void ifBAndCIsEqualToNull(double b, double c) {
+        if (b == 0 && c == 0) {
+            System.out.println("There is one root: 0");
+        }
+    }
+
+    // If only c = 0 the quadratic equation has two roots.
+    private void ifOnlyCEqualToNull(double a, double b, double c) {
+        if (b != 0 && c == 0) {
+            double root1 = 0;
+            double root2 = -b / a;
+            System.out.println("There are two roots: " + root1 + " and " + root2);
+        }
+    }
+
+    // If only b = 0.
+    private void ifOnlyBEqualToNull(double a, double b, double c) {
+        if (b == 0 && c != 0) {
+            // and square root with -b / a more than 0, the quadratic equation has two roots.
+            if (Math.sqrt(-b / a) > 0) {
+                double root1 = Math.sqrt(-c / a);
+                double root2 = -Math.sqrt(-c / a);
+                System.out.println("There are two roots: " + root1 + " and " + root2);
+                // If square root with -b / a few than 0, the quadratic are no real roots.
+            } else {
+                System.out.println("There are no real roots");
+            }
+        }
+    }
+
+    //If all values are not equal to 0
+    private void ifAllIsNotNull(double a, double b, double c) {
+        if (a !=0 && b != 0 && c != 0 ){
+            //calculate the discriminant.
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0) {
+                println("There are no real roots");
+                // if discriminant more than zero, he calculate the rots
+            } else {
+                double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+                if (x1 == x2) {
+                    System.out.println("There is one root: " + x1);
                 } else {
-                    double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
-                    double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-
-                    if (x1 == x2) {
-                        System.out.println("There is one root: " + x1);
-                    } else {
-                        System.out.println("There are two roots: " + x1 + " and " + x2);
-                    }
+                    System.out.println("There are two roots: " + x1 + " and " + x2);
                 }
             }
         }
