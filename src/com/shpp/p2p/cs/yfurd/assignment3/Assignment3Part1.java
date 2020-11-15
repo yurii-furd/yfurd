898252a0eac0dfe6ce5fa294fe3dfcca04b2733e
Yurii Furd <yura.furd@gmail.com>
2020-11-15 19:22:08 +0200

    Rafactor

--- src/com/shpp/p2p/cs/yfurd/assignment3/Assignment3Part1.java
+++ src/com/shpp/p2p/cs/yfurd/assignment3/Assignment3Part1.java
@@ -8,35 +8,35 @@ public class Assignment3Part1 extends TextProgram {
     private static final int AEROBIC_DAYS_REQUIRED = 5;
     //The amount of days that doctors recommend at least 40 minutes to maintain low blood pressure.
     private static final int LOW_BLOOD_PRESSURE_DAYS_REQUIRED = 3;
-    // How many days a week.
+    // ow many days a week.
     private static final  int DAYS_OF_THE_WEEK = 7;
 
     public void run() {
 
         int[] minutesPerDay = readInputData();
 
-        int CALC_AEROBIC_DAYS = 0;
-        int CALC_LOW_BLOOD_PRESSURE_DAYS = 0;
+        int countDaysInWhichMoreThan30Minutes = 0;
+        int countDaysInWhichMoreThan40Minutes = 0;
 
         for (int i : minutesPerDay) {
             if (i >= 30) {
-                CALC_AEROBIC_DAYS++;
+                countDaysInWhichMoreThan30Minutes++;
             }
             if (i >= 40) {
-                CALC_LOW_BLOOD_PRESSURE_DAYS++;
+                countDaysInWhichMoreThan40Minutes++;
             }
         }
 
         System.out.println("Cardio vascular health:");
-        if (CALC_AEROBIC_DAYS < AEROBIC_DAYS_REQUIRED) {
-            System.out.println("You needed to train hard for at least " + (AEROBIC_DAYS_REQUIRED - CALC_AEROBIC_DAYS) + " more day(s) a week!");
+        if (countDaysInWhichMoreThan30Minutes < AEROBIC_DAYS_REQUIRED) {
+            System.out.println("You needed to train hard for at least " + (AEROBIC_DAYS_REQUIRED - countDaysInWhichMoreThan30Minutes) + " more day(s) a week!");
         } else {
             System.out.println("Great job! You've done enough exercise for cardio vascular health.");
         }
 
         System.out.println("Blood pressure:");
-        if (CALC_LOW_BLOOD_PRESSURE_DAYS < LOW_BLOOD_PRESSURE_DAYS_REQUIRED) {
-            System.out.println("You needed to train hard for at least " + (LOW_BLOOD_PRESSURE_DAYS_REQUIRED - CALC_LOW_BLOOD_PRESSURE_DAYS) + " more day(s) a week!");
+        if (countDaysInWhichMoreThan40Minutes < LOW_BLOOD_PRESSURE_DAYS_REQUIRED) {
+            System.out.println("You needed to train hard for at least " + (LOW_BLOOD_PRESSURE_DAYS_REQUIRED - countDaysInWhichMoreThan40Minutes) + " more day(s) a week!");
         } else {
             System.out.println("Great job! You've done enough exercise for cardio vascular health.");
         }
