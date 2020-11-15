e978a62fc87738f18bb8ddeea679d22932ed07fc
Yurii Furd <yura.furd@gmail.com>
2020-11-15 15:00:56 +0200

    Rafactor

---
+++ src/com/shpp/p2p/cs/yfurd/assignment3/Assignment3Part2.java
@@ -0,0 +1,10 @@
+package com.shpp.p2p.cs.yfurd.assignment3;
+
+import com.shpp.cs.a.console.TextProgram;
+
+public class Assignment3Part2 extends TextProgram {
+
+    public void run() {
+
+    }
+}
