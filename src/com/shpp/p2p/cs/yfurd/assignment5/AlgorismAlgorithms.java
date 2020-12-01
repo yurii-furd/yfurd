61e9f8b1a3ea201570611270df9e40d8f20e98ba
Yurii Furd <yura.furd@gmail.com>
2020-12-01 20:37:52 +0200

    create task 2

---
+++ src/com/shpp/p2p/cs/yfurd/assignment5/AlgorismAlgorithms.java
@@ -0,0 +1,73 @@
+package com.shpp.p2p.cs.yfurd.assignment5;
+
+import com.shpp.cs.a.console.TextProgram;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgorismAlgorithms extends TextProgram {
+
+    public void run() {
+        /* Sit in a loop, reading numbers and adding them. */
+        while (true) {
+            String n1 = readLine("Enter first number:  ");
+            String n2 = readLine("Enter second number: ");
+            println(n1 + " + " + n2 + " = " + addNumericStrings(n1, n2));
+            println();
+        }
+    }
+
+    /**
+     * Given two string representations of nonnegative integers, adds the
+     * numbers represented by those strings and returns the result.
+     *
+     * @param n1 The first number.
+     * @param n2 The second number.
+     * @return A String representation of n1 + n2
+     */
+    private String addNumericStrings(String n1, String n2) {
+
+        String sum = "";
+        n1 = revers(n1);
+        n2 = revers(n2);
+        char[] nOne = n1.toCharArray();
+        char[] nTwo = n2.toCharArray();
+
+        List<Integer> list = new ArrayList<>();
+
+        int a = 0;
+        for (int i = 0; i < nOne.length; i++) {
+
+            int x = nOne[i] - '0';
+            System.out.println(x);
+            int y = nTwo[i] - '0';
+            System.out.println(y);
+
+            int temp = list.get(i);
+            if (x + y + temp>= 10) {
+                list.add(x + y - 10);
+                a++;
+                list.set(i + 1, a);
+                a = 0;
+            } else {
+                list.add(x + y);
+            }
+
+        }
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        return revers(sum);
+    }
+
+
+    private String revers(String s) {
+        String str = "";
+        char[] ch = s.toCharArray();
+
+        for (int i = ch.length; i > 0; i--) {
+            str += ch[i - 1];
+        }
+        return str;
+    }
+}
