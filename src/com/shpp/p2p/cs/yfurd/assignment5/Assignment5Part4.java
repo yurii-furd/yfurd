48501ae10b5f13aaab3c2f5cc4556bbe4ecaa551
Yurii Furd <yura.furd@gmail.com>
2020-12-04 22:31:12 +0200

    task 1 no comment

--- src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part4.java
+++ src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part4.java
@@ -10,16 +10,25 @@ import java.util.ArrayList;
 public class Assignment5Part4 extends TextProgram {
 
     public void run() {
-        String path1 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
+        ArrayList<String> list;
+        String path1 = "home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
         String path2 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/food-origins.csv";
-        extractColumn(path1, 2);
+        list = extractColumn(path1, 5);
+
+        System.out.print("[");
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                System.out.print(list.get(i) + ".");
+            } else {
+                System.out.print(list.get(i) + ", ");
+            }
+        }
+        System.out.println("]");
     }
 
     private ArrayList<String> extractColumn(String filename, int columnIndex) {
-//        String path1 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
-//        String path2 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/food-origins.csv";
-
         ArrayList<String> list;
+        ArrayList<String> list2 = new ArrayList<>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
             while (true) {
@@ -28,53 +37,32 @@ public class Assignment5Part4 extends TextProgram {
                     break;
                 }
                 list = fieldsIn(str);
-
-                for (
-                        String s :
-                        list) {
-                    System.out.println(s);
-                }
-
-
+                String s = list.get(columnIndex - 1);
+                list2.add(s);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            System.out.println("Wrong file path!");
+            return null;
         }
-
-
-        return null;
+        return list2;
     }
 
     private ArrayList<String> fieldsIn(String line) {
         ArrayList<String> list = new ArrayList<>();
 
         String str = "";
-
         char[] ch = line.toCharArray();
-
         boolean columnWithPaws = false;
+        boolean columnWithoutPaws = false;
 
         for (int i = 0; i < ch.length; i++) {
 
-            if (i == ch[ch.length - 1] && ch[ch.length - 1] == ',' || i == 0 && ch[i] == ',') {
-                str = "пуста колонка ";
-                list.add(str);
-                str = "";
-            }
-
-            if (ch[i] == ',' && i != ch.length - 1 && ch[i + 1] == ',') {
-                str = "пуста колонка посередині ";
-                list.add(str);
-                str = "";
-            }
-
-
             if (i == 1 && ch[i - 1] == '"' ||
                     i > 1 && ch[i - 1] == '"' && ch[i - 2] == ',' ||
-                    columnWithPaws == true
-            ) {
+                    columnWithPaws) {
                 columnWithPaws = true;
-                if (ch[i] != '"' && columnWithPaws == true) {
+                if (ch[i] != '"' && columnWithPaws) {
                     str += ch[i];
                 }
                 if (ch[i] == '"' && ch[i] == ch[ch.length - 1] || ch[i] == '"' && ch[i + 1] == ',') {
@@ -82,13 +70,36 @@ public class Assignment5Part4 extends TextProgram {
                     str = "";
                     columnWithPaws = false;
                 }
-
             }
 
 
+            if (i == 0 && ch[i] != '"' && ch[i] != ',' ||
+                    i > 1 && ch[i] != '"' && ch[i] != ',' && !columnWithPaws ||
+                    columnWithoutPaws) {
 
+                columnWithoutPaws = true;
+                if (ch[i] != ',') {
+                    str += ch[i];
+                }
+                if (ch[i] == ',' || i == ch.length - 1) {
+                    list.add(str);
+                    str = "";
+                    columnWithoutPaws = false;
+                }
+            }
+
+            if (i == ch.length - 1 && ch[ch.length - 1] == ',' || i == 0 && ch[i] == ',') {
+                str = " ";
+                list.add(str);
+                str = "";
+            }
+
+            if (ch[i] == ',' && i != ch.length - 1 && ch[i + 1] == ',') {
+                str = " ";
+                list.add(str);
+                str = "";
+            }
         }
-
         return list;
     }
 }
