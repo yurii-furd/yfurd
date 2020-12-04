34277694c990febd82be02b343e07cd22d4d6275
Yurii Furd <yura.furd@gmail.com>
2020-12-04 14:07:07 +0200

    create task 4

--- src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part4.java
+++ src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part4.java
@@ -10,15 +10,16 @@ import java.util.ArrayList;
 public class Assignment5Part4 extends TextProgram {
 
     public void run() {
+        String path1 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
         String path2 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/food-origins.csv";
-        extractColumn(path2, 2);
+        extractColumn(path1, 2);
     }
 
     private ArrayList<String> extractColumn(String filename, int columnIndex) {
 //        String path1 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
 //        String path2 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/food-origins.csv";
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list;
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
             while (true) {
@@ -28,29 +29,14 @@ public class Assignment5Part4 extends TextProgram {
                 }
                 list = fieldsIn(str);
 
-                String strList = list.get(0);
-                char[] ch = strList.toCharArray();
-                String newStr = "";
-
-                int a = 0;
-                for (int i = 0; i < ch.length - 1; i++) {
-
-                    if (a == 2){
-                        newStr += ch[i];
-                    }
-
-                    if (ch[i] == '"' && a == 1){
-                        a = 2;
-                    }
-
-                    if (ch[i] == ','){
-                        a = 1;
-                    }
-
+                for (
+                        String s :
+                        list) {
+                    System.out.println(s);
                 }
-                System.out.println(newStr);
-            }
 
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -61,8 +47,48 @@ public class Assignment5Part4 extends TextProgram {
 
     private ArrayList<String> fieldsIn(String line) {
         ArrayList<String> list = new ArrayList<>();
-        list.add(line);
+
+        String str = "";
+
+        char[] ch = line.toCharArray();
+
+        boolean columnWithPaws = false;
+
+        for (int i = 0; i < ch.length; i++) {
+
+            if (i == ch[ch.length - 1] && ch[ch.length - 1] == ',' || i == 0 && ch[i] == ',') {
+                str = "пуста колонка ";
+                list.add(str);
+                str = "";
+            }
+
+            if (ch[i] == ',' && i != ch.length - 1 && ch[i + 1] == ',') {
+                str = "пуста колонка посередині ";
+                list.add(str);
+                str = "";
+            }
+
+
+            if (i == 1 && ch[i - 1] == '"' ||
+                    i > 1 && ch[i - 1] == '"' && ch[i - 2] == ',' ||
+                    columnWithPaws == true
+            ) {
+                columnWithPaws = true;
+                if (ch[i] != '"' && columnWithPaws == true) {
+                    str += ch[i];
+                }
+                if (ch[i] == '"' && ch[i] == ch[ch.length - 1] || ch[i] == '"' && ch[i + 1] == ',') {
+                    list.add(str);
+                    str = "";
+                    columnWithPaws = false;
+                }
+
+            }
+
+
+
+        }
+
         return list;
     }
-
 }
