2048bfde54e99d39eeb78c45add3bffd5846b140
Yurii Furd <yura.furd@gmail.com>
2020-12-06 20:59:08 +0200

    Refactor

--- src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part1.java
+++ src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part1.java
@@ -2,6 +2,10 @@ package com.shpp.p2p.cs.yfurd.assignment5;
 
 import com.shpp.cs.a.console.TextProgram;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Assignment5Part1 extends TextProgram {
     public void run() {
         /* Repeatedly prompt the user for a word and print out the estimated
@@ -21,63 +25,33 @@ public class Assignment5Part1 extends TextProgram {
      * @return An estimate of the number of syllables in that word.
      */
     private int syllablesIn(String word) {
-        word = word.toLowerCase();
-        char[] str = word.toCharArray();
-        // array chars with vowels.
-        char[] vowels = {'a', 'i', 'o', 'u', 'y', 'e'};
+        char[] wordCharArray = word.toLowerCase().toCharArray();
+        // List chars with vowels.
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'i', 'o', 'u', 'y', 'e'));
+
         int sum = 0;
-        boolean isE = false;
+        // We run through an array of spells.
+        for (int i = 0; i < wordCharArray.length; i++) {
+            //if a letter with vowels = iteration letter i
+            if (vowels.contains(wordCharArray[i])) {
 
-        for (int i = 0; i < str.length; i++) {
-            for (char vowel : vowels) {
-                if (str[i] == vowel) {
-
-                    if (i == str.length - 1 && str[i] == 'e' && i != 0) {
-                        if (    str[i - 1] != 'a' ||
-                                str[i - 1] != 'i' ||
-                                str[i - 1] != 'o' ||
-                                str[i - 1] != 'u' ||
-                                str[i - 1] != 'y' ||
-                                str[i - 1] != 'e'
-                        ) {
-                            isE = true;
-
-                        }
-                    }
-
+                /*1. if both the element is less than the length of the array and the element i + 1
+                *    does not contain an element from the letter of vowels.
+                * 2. if the last element contains a letter element of vowels and is not equal to the letter e.
+                * 3. if before the last element is equal to a vowel, and the last is equal to the letter e.
+                * then increase the amount by 1
+                */
+                if (i < wordCharArray.length - 1 && !vowels.contains(wordCharArray[i + 1])
+                        || i == wordCharArray.length - 1 && vowels.contains(wordCharArray[i]) && wordCharArray[i] != 'e'
+                        || i == wordCharArray.length - 1  && vowels.contains(wordCharArray[i - 1]) && wordCharArray[i] == 'e') {
                     sum++;
-                    for (char c : vowels) {
-                        if (i + 1 < str.length && str[i + 1] == c) {
-                            sum--;
-                        }
-                    }
                 }
             }
         }
-        if (isE) {
-            sum--;
-            isE = false;
-        }
-
+        //The method must always return at least 1 syllable.
         if (sum < 1) {
-            return 1;
+            sum = 1;
         }
-
         return sum;
     }
 }
-
-/*  !Words to check!
-
-    Unity: 3 syllables
-    Unite: 2 syllables
-    Growth: 1 syllable
-    Possibilities: 5 syllables
-    Nimble: 1 syllable (насправді 2, але наш спрощений алгоритм повинен видати 1)
-    Me: 1 syllable
-    Beautiful: 3 syllables
-    Manatee: 3 syllables
-    quokka — 2 склади,
-    springbok - 2 склади
-    syllable - 2 склади
-*/
