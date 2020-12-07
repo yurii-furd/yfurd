c1fc758f6bc5af379a12f8a36de54bc0698a5b6c
Yurii Furd <yura.furd@gmail.com>
2020-12-07 20:44:30 +0200

    Refactor

--- src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part4.java
+++ src/com/shpp/p2p/cs/yfurd/assignment5/Assignment5Part4.java
@@ -3,103 +3,147 @@ package com.shpp.p2p.cs.yfurd.assignment5;
 import com.shpp.cs.a.console.TextProgram;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Assignment5Part4 extends TextProgram {
 
+    private static final String PATH1 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
+    private static final String PATH2 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/food-origins.csv";
+
     public void run() {
         ArrayList<String> list;
-        String path1 = "home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/car.csv";
-        String path2 = "/home/yurii/development/projects/yfurd/src/com/shpp/p2p/cs/yfurd/assignment5/food-origins.csv";
-        list = extractColumn(path1, 5);
-
-        System.out.print("[");
-        for (int i = 0; i < list.size(); i++) {
-            if (i == list.size() - 1) {
-                System.out.print(list.get(i) + ".");
-            } else {
-                System.out.print(list.get(i) + ", ");
-            }
-        }
-        System.out.println("]");
+        list = extractColumn(PATH1, 0);
+        println(list);
     }
 
+    /**
+     * This method reads the information from the file, selects the necessary and transmits it.
+     *
+     * @param filename file from which the information will be read.
+     * @param columnIndex column from which you want to read the information.
+     * @return return the list with the required columns.
+     */
     private ArrayList<String> extractColumn(String filename, int columnIndex) {
-        ArrayList<String> list;
-        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
-            while (true) {
-                String str = bufferedReader.readLine();
-                if (str == null) {
-                    break;
-                }
-                list = fieldsIn(str);
-                String s = list.get(columnIndex - 1);
-                list2.add(s);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+
+                ArrayList<String> colonsLine = fieldsIn(line);
+                String colonsOfLine = colonsLine.get(columnIndex);
+                result.add(colonsOfLine);
             }
-        } catch (IOException e) {
-//            e.printStackTrace();
-            System.out.println("Wrong file path!");
+        } catch (FileNotFoundException e) {
             return null;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return list2;
+        return result;
     }
 
+    /**
+     * Takes a row with columns and returns a list of columns of one row.
+     * @param line information line.
+     * @return a list of columns in one row.
+     */
     private ArrayList<String> fieldsIn(String line) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> colonsLine = new ArrayList<>();
 
-        String str = "";
-        char[] ch = line.toCharArray();
+        StringBuilder column = new StringBuilder();
+
+        char[] oneLineChar = line.toCharArray();
         boolean columnWithPaws = false;
-        boolean columnWithoutPaws = false;
 
-        for (int i = 0; i < ch.length; i++) {
+        // Run along the data line.
+        for (int i = 0; i < oneLineChar.length; i++) {
 
-            if (i == 1 && ch[i - 1] == '"' ||
-                    i > 1 && ch[i - 1] == '"' && ch[i - 2] == ',' ||
-                    columnWithPaws) {
+            // If quotation marks are present, change the variable "columnWithPaws" to true
+            if (isStartPaws(i, oneLineChar)) {
                 columnWithPaws = true;
-                if (ch[i] != '"' && columnWithPaws) {
-                    str += ch[i];
-                }
-                if (ch[i] == '"' && ch[i] == ch[ch.length - 1] || ch[i] == '"' && ch[i + 1] == ',') {
-                    list.add(str);
-                    str = "";
-                    columnWithPaws = false;
+            }
+
+            /* If the column with quotes closes, change the variable "columnWithPaws" to false
+            add the read data to the letter */
+            if (isClosePaws(i, oneLineChar)) {
+                columnWithPaws = false;
+                colonsLine.add(column.toString());
+                column = new StringBuilder();
+            }
+
+            // If quotation marks are present, then read character by character in time.
+            if (columnWithPaws) {
+                column.append(oneLineChar[i]);
+            }
+
+            /* Read the information if there is no column with quotation marks and the data i
+            the element are not equal to quotation marks and commas.*/
+            if (!columnWithPaws && oneLineChar[i] != ',' && oneLineChar[i] != '"') {
+                column.append(oneLineChar[i]);
+            }
+
+            /* Add information to the list if we are not in the column with quotation marks,
+            and this element is equal to a comma*/
+            if (!columnWithPaws && oneLineChar[i] == ',') {
+                if (column.length() > 0) {
+                    colonsLine.add(column.toString());
+                    column = new StringBuilder();
                 }
             }
 
-
-            if (i == 0 && ch[i] != '"' && ch[i] != ',' ||
-                    i > 1 && ch[i] != '"' && ch[i] != ',' && !columnWithPaws ||
-                    columnWithoutPaws) {
-
-                columnWithoutPaws = true;
-                if (ch[i] != ',') {
-                    str += ch[i];
-                }
-                if (ch[i] == ',' || i == ch.length - 1) {
-                    list.add(str);
-                    str = "";
-                    columnWithoutPaws = false;
-                }
-            }
-
-            if (i == ch.length - 1 && ch[ch.length - 1] == ',' || i == 0 && ch[i] == ',') {
-                str = " ";
-                list.add(str);
-                str = "";
-            }
-
-            if (ch[i] == ',' && i != ch.length - 1 && ch[i + 1] == ',') {
-                str = " ";
-                list.add(str);
-                str = "";
+            /* Checks to see if there is an empty column at the beginning, middle, or end of the line
+            and add to list*/
+            if (isEmptyColumns(i, oneLineChar)) {
+                colonsLine.add(column.toString());
+                column = new StringBuilder();
             }
         }
-        return list;
+
+        // If the column contains some information then add it to the list
+        if (column.length() > 0) {
+            colonsLine.add(column.toString());
+        }
+        return colonsLine;
+    }
+
+    /**
+     * This method checks whether the column starts with quotation marks or not.
+     *
+     * @param i           iteration on a line with data.
+     * @param oneLineChar a line with data.
+     * @return returns the result of whether the column with quotation marks begins or not.
+     */
+    private boolean isStartPaws(int i, char[] oneLineChar) {
+        return i == 1 && oneLineChar[i - 1] == '"'
+                || i > 1 && oneLineChar[i - 1] == '"' && oneLineChar[i - 2] == ',';
+    }
+
+    /**
+     * This method checks to see if the column ends with quotation marks.
+     *
+     * @param i           iteration on a line with data.
+     * @param oneLineChar a line with data.
+     * @return the result or the column ends with quotation marks.
+     */
+    private boolean isClosePaws(int i, char[] oneLineChar) {
+        return i > 0 && i == oneLineChar.length - 1 && oneLineChar[i] == '"'
+                || i < oneLineChar.length - 1 && oneLineChar[i] == '"' && oneLineChar[i + 1] == ',';
+    }
+
+    /**
+     * This method checks to see if there is an empty column at the beginning, middle, or end of the line.
+     *
+     * @param i           iteration on a line with data.
+     * @param oneLineChar a line with data.
+     * @return the result if there is an empty column in the row.
+     */
+    private boolean isEmptyColumns(int i, char[] oneLineChar) {
+        return i == 0 && oneLineChar[i] == ','
+                || i == oneLineChar.length - 1 && oneLineChar[i] == ','
+                || i > 0 && i < oneLineChar.length - 1 && oneLineChar[i] == ',' && oneLineChar[i + 1] == ',';
     }
 }
