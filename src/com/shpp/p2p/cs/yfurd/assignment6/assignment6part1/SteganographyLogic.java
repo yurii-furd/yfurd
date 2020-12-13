3cbac6ab2cae648d299ba70db33f482d1c2725af
Yurii Furd <yura.furd@gmail.com>
2020-12-13 18:13:19 +0200

    refactor

--- src/com/shpp/p2p/cs/yfurd/assignment6/assignment6part1/SteganographyLogic.java
+++ src/com/shpp/p2p/cs/yfurd/assignment6/assignment6part1/SteganographyLogic.java
@@ -18,17 +18,10 @@ public class SteganographyLogic {
     public static boolean[][] findMessage(GImage source) {
         int[][] pixelArray = source.getPixelArray();
         boolean[][] pixelBlackOrWhite = new boolean[pixelArray.length][pixelArray[0].length];
-
         for (int i = 0; i < pixelArray.length; i++) {
             for (int j = 0; j < pixelArray[i].length; j++) {
-
                 int red = GImage.getRed(pixelArray[i][j]);
-
-                if (red % 2 == 1) {
-                    pixelBlackOrWhite[i][j] = true;
-                } else {
-                    pixelBlackOrWhite[i][j] = false;
-                }
+                pixelBlackOrWhite[i][j] = red % 2 == 1;
             }
         }
         return pixelBlackOrWhite;
@@ -55,35 +48,36 @@ public class SteganographyLogic {
      */
     public static GImage hideMessage(boolean[][] message, GImage source) {
         int[][] pixelArray = source.getPixelArray();
-
         for (int i = 0; i < message.length; i++) {
             for (int j = 0; j < message[i].length; j++) {
-
                 int red = GImage.getRed(pixelArray[i][j]);
-                boolean isBlack = false;
-
-                if (message[i][j]) {
-                    isBlack = true;
-                    red = checkRed(red, isBlack);
-                } else {
-                    isBlack = false;
-                    red = checkRed(red, isBlack);
-                }
-
-                pixelArray[i][j] = GImage.createRGBPixel(red,
+                //if message[i][j] is true -> black, else -> false
+                boolean isBlack = message[i][j];
+                red = checkAndSetRed(red, isBlack);
+                pixelArray[i][j] = GImage.createRGBPixel(
+                        red,
                         GImage.getGreen(pixelArray[i][j]),
-                        GImage.getBlue(pixelArray[i][j]));
+                        GImage.getBlue(pixelArray[i][j]),
+                        GImage.getAlpha(pixelArray[i][j])
+                );
             }
         }
         return new GImage(pixelArray);
     }
 
-    private static int checkRed(int red, boolean isBlack) {
-        if (red % 2 == 0 && isBlack && red != 0 || red % 2 == 1 && !isBlack && red != 0) {
-            return red - 1;
-        } else if (red % 2 == 0 && isBlack && red != 255 || red % 2 == 1 && !isBlack && red != 255) {
-            return red + 1;
+    /**
+     * Depending on the input pixel color, this method makes the color even or not even.
+     *
+     * @param red     red value in the pixel.
+     * @param isBlack contains black or red.
+     * @return editable red pixel particle.
+     */
+    private static int checkAndSetRed(int red, boolean isBlack) {
+        if (isBlack) {
+            return red | 1;
+        } else {
+            return red & 254;
         }
-        return red;
     }
 }
+
