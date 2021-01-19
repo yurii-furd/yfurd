d3852a558fbb437dace42c5dc08d10d39bd2a975
Yurii Furd <yura.furd@gmail.com>
2021-01-19 17:45:24 +0200

    Added processing logic and adding input variables with "args"

--- src/com/shpp/p2p/cs/yfurd/assignment10/Assignment10Part1.java
+++ src/com/shpp/p2p/cs/yfurd/assignment10/Assignment10Part1.java
@@ -13,9 +13,9 @@ public class Assignment10Part1 {
     static Set<Character> letters = new HashSet<>(Arrays.asList('q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y',
             'u', 'U', 'i', 'I', 'o', 'O', 'p', 'P', 'a', 'A', 's', 'S', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H',
             'j', 'J', 'k', 'K', 'l', 'L', 'z', 'Z', 'x', 'X', 'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M'));
-    static List<Double> list = new ArrayList<>();
-    static List<String> list1 = new ArrayList<>();
-    static List<String> list2 = new ArrayList<>();
+    static List<Double> listNumber = new ArrayList<>();
+    static HashMap<String, Double> mapVariable = new HashMap<>();
+    static List<String> listOperators = new ArrayList<>();
     static StringBuilder numberStr = new StringBuilder();
     static char[] formCh;
     static int a = 1;
@@ -25,6 +25,7 @@ public class Assignment10Part1 {
         System.out.println(args[0].trim());
         StringBuilder newForm = removeSpaces(args[0]);
         System.out.println(newForm);
+        System.out.println(args.length);
 
         formCh = newForm.toString().toCharArray();
 
@@ -33,16 +34,55 @@ public class Assignment10Part1 {
 
                 findNegativeNumber(i);
                 findPositiveNumber(i);
-                findNegativeVariable(i);
+//                findNegativeVariable(i);
                 findPositiveVariable(i);
                 findOperator(i);
                 checkErrorInFormula(i);
 
             }
         }
-        System.out.println(list);
-        System.out.println(list1);
-        System.out.println(list2);
+        System.out.println(mapVariable.size());
+        System.out.println(mapVariable);
+        System.out.println(listNumber);
+        System.out.println(listOperators);
+        System.out.println(args.length + " SSSSSSSSSS");
+
+        if (args.length - mapVariable.size() == 1) {
+            StringBuilder temp = new StringBuilder();
+            StringBuilder tempValueDouble = new StringBuilder();
+
+            for (int i = 1; i < args.length; i++) {
+                StringBuilder param = removeSpaces(args[i]);
+                formCh = param.toString().toCharArray();
+
+                for (int j = 0; j < formCh.length; j++) {
+
+                    if (j == 0 && letters.contains(formCh[j]) || positiveVariable) {
+                        positiveVariable = true;
+                        temp.append(formCh[j]);
+
+                        if (formCh[j + 1] == '=') {
+                            positiveVariable = false;
+                        }
+                    }
+                    if (j > 0 && formCh[j - 1] == '=' && numbers.contains(formCh[j]) || positiveNumber){
+                        positiveNumber = true;
+                        checkDot(formCh[j]);
+                        tempValueDouble.append(formCh[j]);
+                        if (j == formCh.length - 1){
+                            positiveNumber = false;
+                            mapVariable.put(temp.toString(), Double.parseDouble(tempValueDouble.toString()));
+                            a = 1;
+                            temp = new StringBuilder();
+                            tempValueDouble = new StringBuilder();
+                        }
+                    }
+                }
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+        System.out.println(mapVariable);
     }
 
     private static void checkErrorInFormula(int i) {
@@ -56,7 +96,7 @@ public class Assignment10Part1 {
     private static void findOperator(int i) {
         if (i >= 1 && i <= formCh.length - 1 && operators.contains(formCh[i]) && !negativeNumber && !negativeVariable
                 || i >= 1 && i <= formCh.length - 1 && formCh[i] == '-' && !negativeNumber && !negativeVariable) {
-            list2.add(numberStr.append(formCh[i]).toString());
+            listOperators.add(numberStr.append(formCh[i]).toString());
             numberStr = new StringBuilder();
         }
     }
@@ -68,6 +108,8 @@ public class Assignment10Part1 {
                 || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '-' && numbers.contains(formCh[i - 2]) && formCh.length > 3
                 || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '*' && formCh.length > 3
                 || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '/' && formCh.length > 3
+                || i >= 3 && letters.contains(formCh[i]) && formCh[i - 1] == '-' && formCh[i - 2] == '^' && formCh.length > 4
+                || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '^' && formCh.length > 3
                 || positiveVariable
         ) {
             positiveVariable = true;
@@ -79,7 +121,7 @@ public class Assignment10Part1 {
                     || i == formCh.length - 1 && numbers.contains(formCh[i])
             ) {
                 positiveVariable = false;
-                list1.add(numberStr.toString());
+                mapVariable.put(numberStr.toString(), null);
                 numberStr = new StringBuilder();
             }
         }
@@ -101,7 +143,7 @@ public class Assignment10Part1 {
                     || i == formCh.length - 1 && numbers.contains(formCh[i])
             ) {
                 negativeVariable = false;
-                list1.add(numberStr.toString());
+                mapVariable.put(numberStr.toString(), null);
                 numberStr = new StringBuilder();
             }
         }
@@ -118,7 +160,7 @@ public class Assignment10Part1 {
                 || positiveNumber
         ) {
             positiveNumber = true;
-            checkDot(formCh[i], a);
+            checkDot(formCh[i]);
             numberStr.append(formCh[i]);
 
             if (i < formCh.length - 1 && !numbers.contains(formCh[i + 1])
@@ -126,7 +168,7 @@ public class Assignment10Part1 {
             ) {
                 positiveNumber = false;
                 a = 1;
-                list.add((Double.parseDouble(numberStr.toString())));
+                listNumber.add((Double.parseDouble(numberStr.toString())));
                 numberStr = new StringBuilder();
             }
         }
@@ -141,7 +183,7 @@ public class Assignment10Part1 {
                 || negativeNumber
         ) {
             negativeNumber = true;
-            checkDot(formCh[i], a);
+            checkDot(formCh[i]);
             numberStr.append(formCh[i]);
 
             if (i < formCh.length - 1 && !numbers.contains(formCh[i + 1])
@@ -149,13 +191,13 @@ public class Assignment10Part1 {
             ) {
                 negativeNumber = false;
                 a = 1;
-                list.add((Double.parseDouble(numberStr.toString())) * -1.0);
+                listNumber.add((Double.parseDouble(numberStr.toString())) * -1.0);
                 numberStr = new StringBuilder();
             }
         }
     }
 
-    private static void checkDot(char ch, int a) {
+    private static void checkDot(char ch) {
         if (ch == '.' && a == 1) {
             a = 2;
         }
