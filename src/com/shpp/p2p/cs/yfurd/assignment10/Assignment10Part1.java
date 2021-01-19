1c1199c01e7092e2a9ad2bd0c0a6cfb6dfcaa216
Yurii Furd <yura.furd@gmail.com>
2021-01-19 15:57:24 +0200

    Refactor. The logic of each ifa made a separate method.

--- src/com/shpp/p2p/cs/yfurd/assignment10/Assignment10Part1.java
+++ src/com/shpp/p2p/cs/yfurd/assignment10/Assignment10Part1.java
@@ -1,123 +1,158 @@
 package com.shpp.p2p.cs.yfurd.assignment10;
 
 import java.util.*;
-//"(-   1   +   (-valujk6459l6e)   * ((  2  - 2  ^(-  10)) + 50) - a)/-2 *a"
+
 public class Assignment10Part1 {
 
+    static boolean negativeNumber = false;
+    static boolean positiveNumber = false;
+    static boolean negativeVariable = false;
+    static boolean positiveVariable = false;
+    static Set<Character> operators = new HashSet<>(Arrays.asList('+', '*', '/', '^', '.', '_', '$'));
+    static Set<Character> numbers = new HashSet<>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'));
+    static Set<Character> letters = new HashSet<>(Arrays.asList('q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y',
+            'u', 'U', 'i', 'I', 'o', 'O', 'p', 'P', 'a', 'A', 's', 'S', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H',
+            'j', 'J', 'k', 'K', 'l', 'L', 'z', 'Z', 'x', 'X', 'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M'));
+    static List<Double> list = new ArrayList<>();
+    static List<String> list1 = new ArrayList<>();
+    static List<String> list2 = new ArrayList<>();
+    static StringBuilder numberStr = new StringBuilder();
+    static char[] formCh;
+    static int a = 1;
+
+
     public static void main(String[] args) {
-        Set<Character> numbers = new HashSet<>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'));
-        Set<Character> letters = new HashSet<>(Arrays.asList('q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y',
-                'u', 'U', 'i', 'I', 'o', 'O', 'p', 'P', 'a', 'A', 's', 'S', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H',
-                'j', 'J', 'k', 'K', 'l', 'L', 'z', 'Z', 'x', 'X', 'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M'));
-
-        Set<Character> operators = new HashSet<>(Arrays.asList('+', '-', '*', '/', '^', '.', '(', ')'));
-
-        List<Double> list = new ArrayList<>();
-        List<String> list1 = new ArrayList<>();
-
         System.out.println(args[0].trim());
-
         StringBuilder newForm = removeSpaces(args[0]);
-
         System.out.println(newForm);
 
-        char[] newFormCh = newForm.toString().toCharArray();
+        formCh = newForm.toString().toCharArray();
 
-        boolean negativeNumber = false;
-        boolean positiveNumber = false;
-        boolean negativeVariable = false;
-        boolean positiveVariable = false;
+        if (formCh.length != 0) {
+            for (int i = 0; i < formCh.length; i++) {
 
-        StringBuilder numberStr = new StringBuilder();
-        int a = 1;
+                findNegativeNumber(i);
+                findPositiveNumber(i);
+                findNegativeVariable(i);
+                findPositiveVariable(i);
+                findOperator(i);
+                checkErrorInFormula(i);
 
-        if (newFormCh.length != 0) {
-            for (int i = 0; i < newFormCh.length; i++) {
-
-                if (i == 1 && newFormCh[i - 1] == '-' && numbers.contains(newFormCh[i]) && newFormCh.length > 2
-                        || i >= 3 && newFormCh[i - 1] == '-' && newFormCh[i - 2] == '*' && numbers.contains(newFormCh[i]) && newFormCh.length > 4
-                        || i >= 3 && newFormCh[i - 1] == '-' && newFormCh[i - 2] == '/' && numbers.contains(newFormCh[i]) && newFormCh.length > 4
-                        || i >= 2 && newFormCh[i - 1] == '-' && newFormCh[i - 2] == '+' && numbers.contains(newFormCh[i]) && newFormCh.length > 3
-                        || negativeNumber
-                ) {
-                    negativeNumber = true;
-                    checkDot(newFormCh[i], a);
-                    numberStr.append(newFormCh[i]);
-
-                    if (i < newFormCh.length - 1 && !numbers.contains(newFormCh[i + 1])
-                            || i == newFormCh.length - 1 && numbers.contains(newFormCh[i])
-                    ) {
-                        negativeNumber = false;
-                        a = 1;
-                        list.add((Double.parseDouble(numberStr.toString())) * -1.0);
-                        numberStr = new StringBuilder();
-                    }
-                }
-
-                if (i == 0 && numbers.contains(newFormCh[i])
-                        || i >= 1 && numbers.contains(newFormCh[i]) && newFormCh[i - 1] == '+' && newFormCh.length > 2
-                        || i >= 2 && numbers.contains(newFormCh[i]) && newFormCh[i - 1] == '-' && numbers.contains(newFormCh[i - 2]) && newFormCh.length > 3
-                        || i >= 2 && numbers.contains(newFormCh[i]) && newFormCh[i - 1] == '-' && letters.contains(newFormCh[i - 2]) && newFormCh.length > 3
-                        || i >= 2 && numbers.contains(newFormCh[i]) && newFormCh[i - 1] == '*' && newFormCh.length > 3
-                        || i >= 2 && numbers.contains(newFormCh[i]) && newFormCh[i - 1] == '/' && newFormCh.length > 3
-                        || positiveNumber
-                ) {
-                    positiveNumber = true;
-                    checkDot(newFormCh[i], a);
-                    numberStr.append(newFormCh[i]);
-
-                    if (i < newFormCh.length - 1 && !numbers.contains(newFormCh[i + 1])
-                            || i == newFormCh.length - 1 && numbers.contains(newFormCh[i])
-                    ) {
-                        positiveNumber = false;
-                        a = 1;
-                        list.add((Double.parseDouble(numberStr.toString())));
-                        numberStr = new StringBuilder();
-                    }
-                }
-
-                if (i == 0 && newFormCh[i] == '-' && letters.contains(newFormCh[i + 1]) && newFormCh.length > 2
-                        || i >= 2 && newFormCh[i] == '-' && newFormCh[i - 1] == '*' && letters.contains(newFormCh[i + 1]) && newFormCh.length > 4
-                        || i >= 2 && newFormCh[i] == '-' && newFormCh[i - 1] == '/' && letters.contains(newFormCh[i + 1]) && newFormCh.length > 4
-                        || i >= 1 && newFormCh[i] == '-' && newFormCh[i - 1] == '+' && letters.contains(newFormCh[i + 1]) && newFormCh.length > 3
-                        || negativeVariable
-                ) {
-                    negativeVariable = true;
-                    numberStr.append(newFormCh[i]);
-
-                    if (i < newFormCh.length - 1 && operators.contains(newFormCh[i + 1])
-                    || i == newFormCh.length - 1 && letters.contains(newFormCh[i])
-                    || i == newFormCh.length - 1 && numbers.contains(newFormCh[i])
-                    ) {
-                        negativeVariable = false;
-                        list1.add(numberStr.toString());
-                        numberStr = new StringBuilder();
-                    }
-                }
-
-                if (i == 0 && letters.contains(newFormCh[i])
-                        || i >= 1 && letters.contains(newFormCh[i]) && newFormCh[i - 1] == '+' && newFormCh.length > 2
-                        || i >= 2 && letters.contains(newFormCh[i]) && newFormCh[i - 1] == '-' && letters.contains(newFormCh[i - 2]) && newFormCh.length > 3
-                        || i >= 2 && letters.contains(newFormCh[i]) && newFormCh[i - 1] == '-' && numbers.contains(newFormCh[i - 2]) && newFormCh.length > 3
-                        || i >= 2 && letters.contains(newFormCh[i]) && newFormCh[i - 1] == '*' && newFormCh.length > 3
-                        || i >= 2 && letters.contains(newFormCh[i]) && newFormCh[i - 1] == '/' && newFormCh.length > 3
-                        || positiveVariable
-                ) {
-                    positiveVariable = true;
-                    numberStr.append(newFormCh[i]);
-
-                    if (i < newFormCh.length - 1 && operators.contains(newFormCh[i + 1])
-                            || i == newFormCh.length - 1 && letters.contains(newFormCh[i])
-                            || i == newFormCh.length - 1 && numbers.contains(newFormCh[i])) {
-                        positiveVariable = false;
-                        list1.add(numberStr.toString());
-                        numberStr = new StringBuilder();
-                    }
-                }
             }
         }
         System.out.println(list);
         System.out.println(list1);
+        System.out.println(list2);
+    }
+
+    private static void checkErrorInFormula(int i) {
+        if (!numbers.contains(formCh[i]) && !operators.contains(formCh[i]) && !letters.contains(formCh[i]) && formCh[i] != '-'
+                || operators.contains(formCh[i]) && operators.contains(formCh[i + 1]) && !positiveVariable && !negativeVariable
+        ) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void findOperator(int i) {
+        if (i >= 1 && i <= formCh.length - 1 && operators.contains(formCh[i]) && !negativeNumber && !negativeVariable
+                || i >= 1 && i <= formCh.length - 1 && formCh[i] == '-' && !negativeNumber && !negativeVariable) {
+            list2.add(numberStr.append(formCh[i]).toString());
+            numberStr = new StringBuilder();
+        }
+    }
+
+    private static void findPositiveVariable(int i) {
+        if (i == 0 && letters.contains(formCh[i])
+                || i >= 1 && letters.contains(formCh[i]) && formCh[i - 1] == '+' && formCh.length > 2
+                || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '-' && letters.contains(formCh[i - 2]) && formCh.length > 3
+                || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '-' && numbers.contains(formCh[i - 2]) && formCh.length > 3
+                || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '*' && formCh.length > 3
+                || i >= 2 && letters.contains(formCh[i]) && formCh[i - 1] == '/' && formCh.length > 3
+                || positiveVariable
+        ) {
+            positiveVariable = true;
+            numberStr.append(formCh[i]);
+
+            if (i < formCh.length - 1 && operators.contains(formCh[i + 1])
+                    || i < formCh.length - 1 && formCh[i + 1] == '-'
+                    || i == formCh.length - 1 && letters.contains(formCh[i])
+                    || i == formCh.length - 1 && numbers.contains(formCh[i])
+            ) {
+                positiveVariable = false;
+                list1.add(numberStr.toString());
+                numberStr = new StringBuilder();
+            }
+        }
+    }
+
+    private static void findNegativeVariable(int i) {
+        if (i == 0 && formCh[i] == '-' && letters.contains(formCh[i + 1]) && formCh.length > 2
+                || i >= 2 && formCh[i] == '-' && formCh[i - 1] == '*' && letters.contains(formCh[i + 1]) && formCh.length > 4
+                || i >= 2 && formCh[i] == '-' && formCh[i - 1] == '/' && letters.contains(formCh[i + 1]) && formCh.length > 4
+                || i >= 1 && formCh[i] == '-' && formCh[i - 1] == '+' && letters.contains(formCh[i + 1]) && formCh.length > 3
+                || i >= 2 && formCh[i] == '-' && formCh[i - 1] == '^' && letters.contains(formCh[i + 1]) && formCh.length > 4
+                || negativeVariable
+        ) {
+            negativeVariable = true;
+            numberStr.append(formCh[i]);
+
+            if (i < formCh.length - 1 && operators.contains(formCh[i + 1])
+                    || i == formCh.length - 1 && letters.contains(formCh[i])
+                    || i == formCh.length - 1 && numbers.contains(formCh[i])
+            ) {
+                negativeVariable = false;
+                list1.add(numberStr.toString());
+                numberStr = new StringBuilder();
+            }
+        }
+    }
+
+    private static void findPositiveNumber(int i) {
+        if (i == 0 && numbers.contains(formCh[i])
+                || i >= 1 && numbers.contains(formCh[i]) && formCh[i - 1] == '+' && formCh.length > 2
+                || i >= 2 && numbers.contains(formCh[i]) && formCh[i - 1] == '-' && numbers.contains(formCh[i - 2]) && formCh.length > 3
+                || i >= 2 && numbers.contains(formCh[i]) && formCh[i - 1] == '-' && letters.contains(formCh[i - 2]) && formCh.length > 3
+                || i >= 2 && numbers.contains(formCh[i]) && formCh[i - 1] == '*' && formCh.length > 3
+                || i >= 2 && numbers.contains(formCh[i]) && formCh[i - 1] == '/' && formCh.length > 3
+                || i >= 2 && numbers.contains(formCh[i]) && formCh[i - 1] == '^' && formCh.length > 3
+                || positiveNumber
+        ) {
+            positiveNumber = true;
+            checkDot(formCh[i], a);
+            numberStr.append(formCh[i]);
+
+            if (i < formCh.length - 1 && !numbers.contains(formCh[i + 1])
+                    || i == formCh.length - 1 && numbers.contains(formCh[i])
+            ) {
+                positiveNumber = false;
+                a = 1;
+                list.add((Double.parseDouble(numberStr.toString())));
+                numberStr = new StringBuilder();
+            }
+        }
+    }
+
+    private static void findNegativeNumber(int i) {
+        if (i == 1 && formCh[i - 1] == '-' && numbers.contains(formCh[i]) && formCh.length > 2
+                || i >= 3 && formCh[i - 1] == '-' && formCh[i - 2] == '*' && numbers.contains(formCh[i]) && formCh.length > 4
+                || i >= 3 && formCh[i - 1] == '-' && formCh[i - 2] == '/' && numbers.contains(formCh[i]) && formCh.length > 4
+                || i >= 2 && formCh[i - 1] == '-' && formCh[i - 2] == '+' && numbers.contains(formCh[i]) && formCh.length > 3
+                || i >= 3 && formCh[i - 1] == '-' && formCh[i - 2] == '^' && numbers.contains(formCh[i]) && formCh.length > 4
+                || negativeNumber
+        ) {
+            negativeNumber = true;
+            checkDot(formCh[i], a);
+            numberStr.append(formCh[i]);
+
+            if (i < formCh.length - 1 && !numbers.contains(formCh[i + 1])
+                    || i == formCh.length - 1 && numbers.contains(formCh[i])
+            ) {
+                negativeNumber = false;
+                a = 1;
+                list.add((Double.parseDouble(numberStr.toString())) * -1.0);
+                numberStr = new StringBuilder();
+            }
+        }
     }
 
     private static void checkDot(char ch, int a) {
