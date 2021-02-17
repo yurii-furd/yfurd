23026badaf15cadbaee5c24f97a6cdf0860af5c3
Yurii Furd <yura.furd@gmail.com>
2021-02-17 20:25:05 +0200

    Refactor Сhanged the algorithm for finding silhouettes in the photo.

--- src/com/shpp/p2p/cs/yfurd/assignment12/Assignment12Part1.java
+++ src/com/shpp/p2p/cs/yfurd/assignment12/Assignment12Part1.java
@@ -5,75 +5,90 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Assignment12Part1 {
-    //    final static String path = "src/com/shpp/p2p/cs/yfurd/assignment12/test/1.png";
-//        final static String path2 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/2.png";
-//    final static String path3 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/3.png";
-//    final static String path4 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/4.png";
-//    final static String path5 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/5.png";
-//        final static String path6 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/6.png";
-//    final static String path7 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/7.png";
-//    final static String path8 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/8.png";
-//    final static String path9 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/9.png";
-//    final static String path10 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/10.png";
-//    final static String path11 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/11.png";
-//    final static String path12 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/12.png";
-//    final static String path13 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/13.png";
-//    final static String path14 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/14.jpg";
-    final static String path15 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/14.png";
-//    final static String test = "src/com/shpp/p2p/cs/yfurd/assignment12/test/test.png";
+    final static String path = "src/com/shpp/p2p/cs/yfurd/assignment12/test/1.png";//true 5
+    final static String path2 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/2.png";//true 36
+    final static String path3 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/3.png";//true 5
+    final static String path4 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/4.png";//true 9 + 1  !!
+    final static String path5 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/5.png";//true 5 + 1  !!
+    final static String path6 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/6.png";//true 8
+    final static String path7 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/7.png";//true 17
+    final static String path8 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/8.png";//false
+    final static String path9 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/9.png";//true 9
+    final static String path10 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/10.png";//false
+    final static String path11 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/11.png";//false
+    final static String path12 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/12.png";//true 36
+    final static String path13 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/13.png";//false
+    final static String path14 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/14.jpg";//true 77
+    final static String path15 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/15.png";//true 77
+    final static String path16 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/16.png";
+    final static String path17 = "src/com/shpp/p2p/cs/yfurd/assignment12/test/small.png";
+    final static Set colorBlack = groupColor();
 
-    public static void main(String[] args) {
-        File file = new File(path15);
-
+    public static void main(String[] args) {                                          //-Xss256m
+        Assignment12Part1 part1 = new Assignment12Part1();
+        File file = new File(path2);
         try {
             BufferedImage image = ImageIO.read(file);
-
-            boolean[][] isTr = new boolean[image.getWidth()][image.getHeight()];
-//            int[][] isTr = new int[image.getWidth()][image.getHeight()];
-
-            Color color1 = new Color(image.getRGB(0, 0), true);
-            Map<Color, Color> map = new HashMap<>();
-
-//            System.out.println(image.getWidth());
-//            System.out.println(image.getHeight());
-
-            for (int i = 0; i < image.getWidth(); i++) {
-                for (int j = 0; j < image.getHeight(); j++) {
-
-                    Color color = new Color(image.getRGB(i, j), true);
-
-                    if (!map.containsKey(color)){
-                        map.put(color, color);
+            boolean[][] isVisited = new boolean[image.getHeight()][image.getWidth()];
+            int countSilhouettes = 0;
+            for (int i = 1; i < image.getHeight() - 1; i++) {
+                for (int j = 1; j < image.getWidth() - 1; j++) {
+                    Color color = new Color(image.getRGB(j, i));
+                    if (colorBlack.contains(color) && !isVisited[i][j]) {
+                        countSilhouettes++;
+                        part1.foundSilhouettes(i, j, image, isVisited);
+                    } else {
+                        isVisited[i][j] = true;
                     }
-
-                    isTr[i][j] = color1.getAlpha() + 100 < color.getAlpha() || color1.getAlpha() - 100 > color.getAlpha();
-//                    if (color1.getAlpha() + 5 < color.getAlpha() || color1.getAlpha() - 5 > color.getAlpha()) {
-//                        isTr[i][j] = 1;
-//                    } else {
-//                        isTr[i][j] = 0;
-//                    }
-
                 }
             }
-
-            System.out.println(map);
-            System.out.println(map.size());
-
-
-//            for (int i = 0; i < isTr.length; i++) {
-//                for (int j = 0; j < isTr[0].length; j++) {
-//                    System.out.print(isTr[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
+            System.out.println(" -> " + countSilhouettes);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * This method recognizes silhouettes in the input image.
+     *
+     * @param i y coordinate of x bricks.
+     * @param j x coordinate of x bricks.
+     * @param image input image.
+     * @param isVisited an array in which the visited and unvisited pixels in the image are marked.
+     */
+    private void foundSilhouettes(int i, int j, BufferedImage image, boolean[][] isVisited) {
+        Color color = new Color(image.getRGB(j, i));
+        if (!isVisited[i][j] && i > 0 && j > 0 && i < image.getHeight() - 1 && j < image.getWidth() - 1 && colorBlack.contains(color)) {
+            isVisited[i][j] = true;
+            foundSilhouettes(i + 1, j, image, isVisited);
+            foundSilhouettes(i + 1, j - 1, image, isVisited);
+            foundSilhouettes(i, j - 1, image, isVisited);
+            foundSilhouettes(i - 1, j - 1, image, isVisited);
+            foundSilhouettes(i - 1, j, image, isVisited);
+            foundSilhouettes(i - 1, j + 1, image, isVisited);
+            foundSilhouettes(i, j + 1, image, isVisited);
+            foundSilhouettes(i + 1, j + 1, image, isVisited);
+        }
+        isVisited[i][j] = true;
+    }
+
+    /**
+     * This method makes similar colors in one sheet.
+     *
+     * @return letter of similar colors.
+     */
+    private static Set groupColor() {
+        Set<Color> set = new HashSet<>();
+        for (int i = 0; i <= 128; i++) {
+            for (int j = 0; j <= 128; j++) {
+                for (int k = 0; k < 128; k++) {
+                    set.add(new Color(i, j, k));
+                }
+            }
+        }
+        return set;
     }
 }
---
+++ src/com/shpp/p2p/cs/yfurd/assignment13/Assignment13Part1.java
@@ -0,0 +1,128 @@
+package com.shpp.p2p.cs.yfurd.assignment13;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class Assignment13Part1 {
+    final static Set colorBlack = groupColor();
+    static int countSilhouettes = 0;
+    static boolean[][] isVisited;
+    static BufferedImage image;
+
+    /**
+     * This method reads the file, runs through each pixel and finds the silhouette.
+     *
+     * @param path file path (photo).
+     */
+    public void readFile(String path) {
+        File file = new File(path);
+        try {
+            image = ImageIO.read(file);
+            isVisited = new boolean[image.getHeight()][image.getWidth()];
+
+            for (int i = 1; i < image.getHeight() - 1; i++) {
+                for (int j = 1; j < image.getWidth() - 1; j++) {
+                    Color color = new Color(image.getRGB(j, i));
+                    if (colorBlack.contains(color) && !isVisited[i][j]) {
+                        countSilhouettes++;
+                        foundSilhouettes(i, j);
+                    } else {
+                        isVisited[i][j] = true;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(" -> " + countSilhouettes);
+        countSilhouettes = 0;
+    }
+
+    /**
+     * This method recognizes silhouettes in the input image.
+     *
+     * @param i     y coordinate of x.
+     * @param j     x coordinate of x.
+     */
+    private void foundSilhouettes(int i, int j) {
+        Color color = new Color(image.getRGB(j, i));
+        Queue<Coordinates> queue = new LinkedList<>();
+        int count = 1;
+        queue.add(new Coordinates(i, j));
+
+        while (!queue.isEmpty()) {
+            Coordinates element = queue.element();
+            if (
+                    !isVisited[element.getX()][element.getY()]
+                            && element.getX() > 0
+                            && element.getY() > 0
+                            && element.getX() < image.getHeight()
+                            && element.getY() < image.getWidth()
+                            && colorBlack.contains(color)
+            ) {
+                isVisited[element.getX()][element.getY()] = true;
+                count++;
+
+                count = findNextPixel(element.getX(), element.getY() + 1, count, queue);
+                count = findNextPixel(element.getX() + 1, element.getY(), count, queue);
+                count = findNextPixel(element.getX(), element.getY() - 1, count, queue);
+                count = findNextPixel(element.getX() - 1, element.getY(), count, queue);
+            }
+            queue.poll();
+        }
+        checkSize(count);
+    }
+
+    /**
+     * This check method means that the required pixel is for the input coordinates.
+     *
+     * @param x  coordinate of x.
+     * @param y  coordinate of y.
+     * @param count counts how many pixels has found about
+     * @param queue in which there are pixels belonging to the silhouette
+     * @return the number of pixels of the found object
+     */
+    private int findNextPixel(int x, int y, int count, Queue queue) {
+        if (!isVisited[x][y] && colorBlack.contains(new Color(image.getRGB(y, x)))) {
+            queue.add(new Coordinates(x, y));
+            count++;
+        }
+            return count;
+    }
+
+    /**
+     * This method checks for the size of the input silhouette,
+     * if it is less than 20 pixels, then this object is not considered a silhouette
+     *
+     * @param count the sum of pixels of one object.
+     */
+    private void checkSize(int count) {
+        if (count < 20) {
+            countSilhouettes--;
+        }
+    }
+
+    /**
+     * This method makes similar colors in one sheet.
+     *
+     * @return letter of similar colors.
+     */
+    private static Set groupColor() {
+        Set<Color> set = new HashSet<>();
+        for (int i = 0; i <= 127; i++) {
+            for (int j = 0; j <= 127; j++) {
+                for (int k = 0; k < 127; k++) {
+                    set.add(new Color(i, j, k));
+                }
+            }
+        }
+        return set;
+    }
+}
